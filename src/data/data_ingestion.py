f977060950701f135df0c10e9671a84adfd13f0b
Rathodpradip1 <pradiprathod45@gmail.com>
2025-09-02 12:08:16 +0530

    CICD run -5

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -75,7 +75,7 @@ def main():
         # test_size = params['data_ingestion']['test_size']
         test_size = 0.2
         
-        # df = load_data(data_url='notebooks/data.csv')
+        #df = load_data(data_url='notebooks/data.csv')
         s3 = s3_connection.s3_operations("capstone-s3-project", "AKIAZZ56MPMUYUVVKLY5", "LTfKEXR80Au6mKcd2uRZWFOVYnbRaRZjfWx7MItV")
         df = s3.fetch_file_from_s3("data.csv")
 
